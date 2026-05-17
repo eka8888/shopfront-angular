e078799bf2945c6b28a8ce981f353f71b80c85f2
Yana Malakhova <ya6ka0@gmail.com>
2026-05-17 16:18:25 +0300

    feat: implement promo-section component

---
+++ src/app/features/home/components/promo-section/promo-section.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-promo-section',
+  imports: [RouterLink],
+  templateUrl: './promo-section.html',
+  styleUrl: './promo-section.scss',
+})
+export class PromoSection {}
--- src/app/features/home/pages/home-page/home-page.ts
+++ src/app/features/home/pages/home-page/home-page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+
 import { ProductCard } from "../../../../shared/components/product-card/product-card";
 import { Product } from '../../../../shared/interfaces/product.interface';
 import { HeroSection } from '../../components/hero-section/hero-section';
 import { CategoriesSection } from '../../components/categories-section/categories-section';
+import { PromoSection } from '../../components/promo-section/promo-section';
 
 @Component({
   selector: 'app-home-page',
-  imports: [ProductCard, HeroSection, CategoriesSection],
+  imports: [ProductCard, HeroSection, CategoriesSection, PromoSection],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
   standalone:true
