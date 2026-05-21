772f08f045494d95ce1952c5f8541985cf588268
S-Arashu <118382286+S-Arashu@users.noreply.github.com>
2026-05-21 23:45:22 +0300

    fix: change styles for buttons and write types in separate file

--- src/app/shared/components/button/button.ts
+++ src/app/shared/components/button/button.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
-
+import { NgClass } from '@angular/common';
+import { ButtonType, ButtonVariant } from '../../types/button.types';
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './button.html',
   styleUrl: './button.scss',
 })
 export class Button {
-  @Input() variant: 'dark' | 'outline' | 'outline-light' | 'text' = 'outline';
-  @Input() type: 'button' | 'submit' = 'button';
+  @Input() variant: ButtonVariant = 'outline';
+  @Input() type: ButtonType = 'button';
   @Input() fullWidth: boolean = false;
 }
---
+++ src/app/shared/types/button.types.ts
@@ -0,0 +1,2 @@
+export type ButtonVariant = 'dark' | 'outline' | 'outline-light' | 'light' | 'text';
+export type ButtonType = 'button' | 'submit';
