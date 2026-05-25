25250a208fcb46a3f4184fa20dbdb7159794d4da
S-Arashu <118382286+S-Arashu@users.noreply.github.com>
2026-05-25 20:02:42 +0300

    fix: add ControlValueAccessor to inputs for interacting with forms

--- src/app/shared/components/input/input.ts
+++ src/app/shared/components/input/input.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 import { InputType, InputVariant } from '../../types/form.enums';
 import { NgClass } from '@angular/common';
 
@@ -7,12 +8,51 @@ import { NgClass } from '@angular/common';
   imports: [NgClass],
   templateUrl: './input.html',
   styleUrl: './input.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
   @Input() label: string = '';
   @Input() placeholder: string = '';
   @Input() type: InputType = InputType.Text;
   @Input() required: boolean = false;
   @Input() error: string = '';
   @Input() variant: InputVariant = InputVariant.Default;
+
+  value: string = '';
+  isDisabled: boolean = false;
+
+  onChange = (value: string) => {};
+  onTouched = () => {};
+
+  writeValue(value: string): void {
+    this.value = value ?? '';
+  }
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+  handleInput(event: Event): void {
+    const value = (event.target as HTMLInputElement).value;
+    this.value = value;
+    this.onChange(value);
+  }
+
+  handleBlur(): void {
+    this.onTouched();
+  }
 }
