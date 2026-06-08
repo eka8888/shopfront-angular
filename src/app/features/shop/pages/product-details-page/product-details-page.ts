b0f55707ca2784a00f0eb9298353787834adec02
Yana Malakhova <ya6ka0@gmail.com>
2026-06-08 23:13:03 +0300

    feat: integrate button component with cart actions

--- src/app/features/shop/pages/product-details-page/product-details-page.ts
+++ src/app/features/shop/pages/product-details-page/product-details-page.ts
@@ -5,16 +5,20 @@ import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../../../../shared/services/product.service';
 import { CartService } from '../../../../shared/services/cart.service';
 import { QuantitySelector } from '../../../../shared/components/quantity-selector/quantity-selector';
+import { Button } from '../../../../shared/components/button/button';
+import { ButtonVariant } from '../../../../shared/types/form.enums';
 
 @Component({
   selector: 'app-product-details-page',
-  imports: [CurrencyPipe, QuantitySelector],
+  imports: [CurrencyPipe, QuantitySelector, Button],
   templateUrl: './product-details-page.html',
 })
 export class ProductDetailsPage {
   private route = inject(ActivatedRoute);
   private productService = inject(ProductService);
   private cartService = inject(CartService);
+
+  readonly ButtonVariant = ButtonVariant;
 
   productId = Number(this.route.snapshot.paramMap.get('id'));
   imagePath = this.productService.getProductDetailsImage(this.productId);
@@ -39,5 +43,13 @@ export class ProductDetailsPage {
 
   onDecreaseQuantity(id: number) {
     this.cartService.decreaseQuantity(id);
+  }
+
+  onAddToCart() {
+    this.cartService.addToCart(this.productId);
+  }
+
+  onRemoveFromCart() {
+    this.cartService.removeFromCart(this.productId);
   }
 }
