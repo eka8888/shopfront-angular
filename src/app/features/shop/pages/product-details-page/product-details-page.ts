20ad0130f25b87a0da3275a3b71f5957a1d78d9e
Yana Malakhova <ya6ka0@gmail.com>
2026-06-08 22:24:29 +0300

    feat: add reusable quantity-selector component to product details

--- src/app/features/shop/pages/product-details-page/product-details-page.ts
+++ src/app/features/shop/pages/product-details-page/product-details-page.ts
@@ -1,16 +1,20 @@
 import { Component, computed, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ProductService } from '../../../../shared/services/product.service';
 import { CurrencyPipe } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+
+import { ProductService } from '../../../../shared/services/product.service';
+import { CartService } from '../../../../shared/services/cart.service';
+import { QuantitySelector } from '../../../../shared/components/quantity-selector/quantity-selector';
 
 @Component({
   selector: 'app-product-details-page',
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, QuantitySelector],
   templateUrl: './product-details-page.html',
 })
 export class ProductDetailsPage {
   private route = inject(ActivatedRoute);
   private productService = inject(ProductService);
+  private cartService = inject(CartService);
 
   productId = Number(this.route.snapshot.paramMap.get('id'));
   imagePath = this.productService.getProductDetailsImage(this.productId);
@@ -19,7 +23,21 @@ export class ProductDetailsPage {
     return this.productService.getProductById(this.productId);
   });
 
+  currentQuantity = computed(() => {
+    const currentItem = this.cartService.cartItems().find((item) => item.id === this.productId);
+
+    return currentItem?.quantity ?? 0;
+  });
+
   oldPrice = computed(() => {
     return this.productService.getOldPrice(this.productId);
   });
+
+  onIncreaseQuantity(id: number) {
+    this.cartService.increaseQuantity(id);
+  }
+
+  onDecreaseQuantity(id: number) {
+    this.cartService.decreaseQuantity(id);
+  }
 }
