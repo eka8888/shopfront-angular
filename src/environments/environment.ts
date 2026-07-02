225bf9b62015a0d9f6a018d6ba2ba4af576eb816
eka8888 <ekandelaki1@gmail.com>
2026-07-02 22:45:47 +0400

    Update remaining Sprint 4 tasks

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,6 @@ export const environment = {
   authUrl: 'https://auth.europe-west1.gcp.commercetools.com',
   apiUrl: 'https://api.europe-west1.gcp.commercetools.com',
 
-  // clientId: '',
-  // clientSecret: '',
-    clientId: 'U8POQ8cOh-GSbi4lFUPyAyXU',
-  clientSecret: 'r7rQfIVczsTLzdBHvuqIGla3NOjBLcAp',
+  clientId: '',
+  clientSecret: ''
 };
