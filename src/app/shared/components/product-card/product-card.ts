bcb43891dc375fad6306df8e27755177e620453d
Yana Malakhova <ya6ka0@gmail.com>
2026-07-11 02:55:36 +0300

    fix: fix linting error

--- src/app/shared/components/product-card/product-card.ts
+++ src/app/shared/components/product-card/product-card.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input, OnDestroy, OnInit, output } from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Product } from '../../interfaces/product.interface';
 import { Button } from '../button/button';
